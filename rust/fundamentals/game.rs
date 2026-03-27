bfaa50027df52b6bc5f8d235acc72cd294a796bc
berdibaevich <berdibaevich.me@gmail.com>
2026-03-27 23:53:44 +0500

    Oops, Error

--- rust/fundamentals/game.rs
+++ rust/fundamentals/game.rs
@@ -1,7 +1,15 @@
+use std::cmp::Ordering;
 use std::io;
+
+use rand::Rng;
+
 
 fn main(){
     println!("Guess the number!");
+
+    let secret_number = rand::thread_rng().gen_range(1..=100);
+
+    println!("The secret number is: {secret_number}");
 
     println!("Please input your guess.");
 
@@ -12,4 +20,11 @@ fn main(){
         .expect("Failed to read line");
 
     println!("Your guessed: {guess}");
+
+    match guess.cmp(&secret_number){
+        Ordering::Less => println!("Too small!"),
+        Ordering::Greater => println!("Too big!"),
+        Ordering::Equal => println!("You Win!"),
+    }
+
 }
